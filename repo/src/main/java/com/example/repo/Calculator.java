eed3b3d4ef5f84c1094387d211037d00e29ad73a
Ольга Артемова <artemovao771@gmail.com>
2024-11-20 23:26:06 +0300

    try

--- repo/src/main/java/com/example/repo/Calculator.java
+++ repo/src/main/java/com/example/repo/Calculator.java
@@ -1,21 +1,52 @@
 package com.example.repo;
 
-public class Calculator {
-    // Метод сложения двух чисел
-    public int add(int a, int b) {
+/**
+ * Класс Calculator предоставляет методы для выполнения базовых арифметических операций.
+ */
+public final class Calculator {
+
+    /**
+     * Метод сложения двух чисел.
+     *
+     * @param a первое число
+     * @param b второе число
+     * @return результат сложения a и b
+     */
+    public final int add(final int a, final int b) {
         return a + b;
     }
 
-    // Вы можете добавить другие методы, например:
-    public int subtract(int a, int b) {
+    /**
+     * Метод вычитания второго числа из первого.
+     *
+     * @param a первое число
+     * @param b второе число
+     * @return результат вычитания b из a
+     */
+    public final int subtract(final int a, final int b) {
         return a - b;
     }
 
-    public int multiply(int a, int b) {
+    /**
+     * Метод умножения двух чисел.
+     *
+     * @param a первое число
+     * @param b второе число
+     * @return результат умножения a и b
+     */
+    public final int multiply(final int a, final int b) {
         return a * b;
     }
 
-    public int divide(int a, int b) throws ArithmeticException {
+    /**
+     * Метод деления первого числа на второе.
+     *
+     * @param a первое число
+     * @param b второе число
+     * @return результат деления a на b
+     * @throws ArithmeticException если b равно нулю
+     */
+    public final int divide(final int a, final int b) throws ArithmeticException {
         if (b == 0) {
             throw new ArithmeticException("Division by zero is not allowed");
         }
